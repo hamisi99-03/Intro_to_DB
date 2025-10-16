3a79bf923ae0c441c7212f046d2224d6d4d07686
Hamisi <yuzalsif2@gmail.com>
2025-10-16 12:42:09 +0300

    made small changes to error handling

--- MySQLServer.py
+++ MySQLServer.py
@@ -16,8 +16,8 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
-        print(f"Error while connecting to MySQL: {e}")
+    except mysql.connector.Error as err:
+       print(f"Error while connecting to MySQL: {err}")
 
     finally:
         if 'cursor' in locals():
